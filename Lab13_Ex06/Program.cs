98acac4085fadcf6986e03ab05e64a831ec586aa
salinatooninter <66030284@kmitl.ac.th>
2025-03-07 16:34:37 +0700

    update

---
+++ Lab13_Ex06/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Animal
+{
+    public string Name { get; set; }
+
+    public Animal(string name)
+    {
+        Name = name;
+        Console.WriteLine($"Animal Constructor: {Name}");
+    }
+}
+
+public class Dog : Animal
+{
+    public string Breed { get; set; }
+
+    public Dog(string name, string breed) : base(name)
+    {
+        Breed = breed;
+        Console.WriteLine($"Dog Constructor: {Name}, {Breed}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dog dog = new Dog("Buddy", "Golden Retriever");
+    }
+}
+
